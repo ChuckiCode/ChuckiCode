8c9e08221989f7d087cb56a4850e7cdf92225d91
chuckicode <mag3davis@gmail.com>
2022-07-22 13:38:35 +0200

    added animation

--- js/app.js
+++ js/app.js
@@ -1,28 +1,16 @@
 "use strict";
 
-
-//////////////////////////////Side menu///////////////////////////////////////////////////
-let openMenu = false; 
-
-function activateMenu(){
-let menu = document.getElementById('sideMnu');
-menu.style.display = 'block';
-
-openMenu = true;
-if(openMenu === true){
-    menu.addEventListener('click', function(){
-        menu.style.display = 'none';
-    });
-}};
-
-const moreBtns = document.querySelectorAll('.moreBtn');
-const asideBlock = document.querySelectorAll('.asideNotActivated');
-const progectWrapers = document.querySelectorAll('.projContainer');
-for(let i = 0; i < moreBtns.length; i++){
-  moreBtns[i].addEventListener('click', function(){
-    moreBtns[i].style.display = 'none';
-    asideBlock[i].classList.add('asideActivated');
-    asideBlock[i].classList.remove('asideNotActivated');
-    progectWrapers[i].style.height = 'fit-content';
+const btnMore = document.querySelectorAll('.moreBtn');
+const cards = document.querySelectorAll('.cordsBox');
+for(let i = 0; i < btnMore.length; i++){
+  btnMore[i].addEventListener('click', function(){
+    // console.log(`pushed button number ${i + 1}`)
+    let userDes = confirm('Apoligies, this button is still under development. Would you like to contact the developer?');
+    if(userDes === true){
+      window.location.href = '../pages/conta.html';
+    } else{
+      window.location.href = '../index.html';
+    }
+    // console.log(cards[i].firstChild)
   })
 }
