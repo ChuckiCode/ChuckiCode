01ccb1ace1fac044980132860d5f7c8fbb482f91
chuckicode <mag3davis@gmail.com>
2022-08-01 16:05:16 +0200

    added context to project page

--- js/app.js
+++ js/app.js
@@ -1,16 +1,1 @@
 "use strict";
-
-const btnMore = document.querySelectorAll('.moreBtn');
-const cards = document.querySelectorAll('.cordsBox');
-for(let i = 0; i < btnMore.length; i++){
-  btnMore[i].addEventListener('click', function(){
-    // console.log(`pushed button number ${i + 1}`)
-    let userDes = confirm('Apoligies, this button is still under development. Would you like to contact the developer?');
-    if(userDes === true){
-      window.location.href = '../pages/conta.html';
-    } else{
-      window.location.href = '../index.html';
-    }
-    // console.log(cards[i].firstChild)
-  })
-}
