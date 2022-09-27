7d46bed77d1f2b5ad4c861ac4e97336e2d6fbe66
Mark <70575298+ChuckiCode@users.noreply.github.com>
2022-09-27 21:08:16 +0200

    Converted website to one page and done some SEO on web page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 "use strict";
+
+let slideIndex = 0;
+
+function startSlide (){
+    const slide = document.querySelectorAll('.designs_responsive');
+    let i;
+    for(i=0;i<slide.length;i++){
+        slide[i].style.display = 'none';
+    }
+
+    slideIndex++;
+    if(slideIndex > slide.length){slideIndex = 1}
+    slide[slideIndex-1].style.display = 'block';
+    setTimeout(startSlide, 3000);
+
+}
+
+startSlide();
